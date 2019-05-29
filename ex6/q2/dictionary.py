0c088182f64876e212a61b262996fc78295789d2
david <davidshato0302@gmail.com>
2019-05-29 18:59:51 +0300

    editing all the file by adding documentation

--- ex6/q2/dictionary.py
+++ ex6/q2/dictionary.py
@@ -5,6 +5,10 @@ import sys
 import os
 import json
 
+
+# geting a string of and counting the amount of appearances of each character
+# and exporing a json file with the results
+
 usage = "USAGE: "+ os.path.basename(sys.argv[0])+" [STRING]"
 
 if len(sys.argv) != 2:
@@ -21,6 +25,7 @@ print(sys.argv)
 string = str(sys.argv[1])
 answer = {}
 
+#iterating on the string and counting the amount of appearances
 for char in string:
     if char in answer:
         answer[char] = answer[char] + 1
@@ -31,6 +36,7 @@ print(answer)
 
 jsonfile = open("dictToJason.json","w")
 
+# convering the dict that containing the answer to json format and writing it to the json file
 jsonfile.write(json.dumps(answer, ensure_ascii=False))
 
 jsonfile.close()
